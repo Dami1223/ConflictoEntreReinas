fef75bede555b99293fbe90368cad8850d9a02fc
Dami1 <Dami1@DAMIN.local>
2021-05-11 18:06:40 -0300

    Actualizar ConflictoEntreReinas.drawio

--- src/ejecucion/EntradaSalida.java
+++ src/ejecucion/EntradaSalida.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Scanner;
 
 import clases.Reina;
--- src/ejecucion/Main.java
+++ src/ejecucion/Main.java
@@ -18,7 +18,15 @@ public class Main {
 		String pathEntrada = "conflictos.in";
 		String pathSalida = "conflictos.out";
 
+		List<Reina> listaDeReinas = ejecutar(pathEntrada, pathSalida);
+
+		System.out.println(listaDeReinas);
+	}
+
+	public static List<Reina> ejecutar(String pathEntrada, String pathSalida)
+			throws NumberFormatException, IOException {
 		List<Reina> listaDeReinas = EntradaSalida.leer(pathEntrada);
+
 		Operacion fila = new Fila();
 		Operacion columna = new Columna();
 		Operacion diagonalPrincipal = new DiagonalPrincipal();
@@ -27,18 +35,16 @@ public class Main {
 		columna.establecerSiguiente(diagonalPrincipal);
 		diagonalPrincipal.establecerSiguiente(diagonalSecundaria);
 		diagonalSecundaria.establecerSiguiente(new SinColision());
-		int contadorIteraciones = 0;
+
 		for (int i = 0; i < listaDeReinas.size() - 1; i++) {
 			for (int j = i + 1; j < listaDeReinas.size(); j++) {
 				fila.comprobarColicion(listaDeReinas.get(i), listaDeReinas.get(j));
-				contadorIteraciones++;
 			}
 		}
-		System.out.println(listaDeReinas);
+
 		EntradaSalida.escribir(pathSalida, listaDeReinas);
-		
-		System.out.println("Se hicieron "+contadorIteraciones+" iteraciones");
-		System.out.println("Reinas ^2 = "+Math.pow(2, listaDeReinas.size()));
+		return listaDeReinas;
+
 	}
 
 }
